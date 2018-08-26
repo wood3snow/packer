f5522c713e3ac14275f63b17368257921fcfe5d6
wood3snow <wood3snow@quietstar.net>
2018-08-27 01:41:55 +0900

    Add CentOS7.5.1804_custom

---
+++ CentOS7.5.1804_custom/packer.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BOX_NAME=CentOS7.5.1804_custom
+
+set -e
+
+if [ -e ~/.vagrant.d/boxes/${BOX_NAME} ]; then
+    rm -rf ~/.vagrant.d/boxes/${BOX_NAME}
+    echo "Deleted Vagrant boxes"
+fi
+
+mkdir -p ../packer_box
+mkdir -p ../packer_cache
+export PACKER_CACHE_DIR="../packer_cache"
+
+packer validate packer.json
+
+packer build -only=virtualbox-iso packer.json
+
+vagrant box add ${BOX_NAME} ../packer_box/${BOX_NAME}.box
